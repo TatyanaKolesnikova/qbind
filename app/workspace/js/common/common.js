9c22203de099fde05457388ffa8b3ddb481ce1a2
Tatyana Kolesnikova <muzychko.tatyana@gmail.com>
2024-04-10 12:57:07 +0300

    change header

--- app/workspace/js/common/common.js
+++ app/workspace/js/common/common.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
-    $(".toogle-menu").click(function () {
+    $(document).on('click', '.toogle-menu', function(e){
         $("body").toggleClass("open-nav");
         return false;
+    });
+    $(document).on('click', '.item-lang', function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        $(this).parents('.hold-lang').toggleClass("open-menu");
+    });
+    $(document).on('click', '.list-lang a', function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        $('.item-lang').text($(this).text());
+        $('.item-lang').attr('data-lang', $(this).attr('data-lang'));
+        $('.list-lang a').removeClass('active');
+        $(this).addClass('active');
+        $(this).parents('.hold-lang').removeClass("open-menu");
     });
     
 });
